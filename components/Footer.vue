f459585d0c5431edc3af02ce8a3874611dd69b0a
Jacky Fan <contact@jacky.fan>
2023-06-17 03:05:57 +0800

    UI: footer

---
+++ components/Footer.vue
@@ -0,0 +1,7 @@
+<template>
+    <footer class="footer footer-center p-4 bg-base-300 text-base-content">
+        <div>
+            <p>Copyright © 2023 - All right reserved by Jacky FAN</p>
+        </div>
+    </footer>
+</template>
