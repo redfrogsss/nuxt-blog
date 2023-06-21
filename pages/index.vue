8d33c4908d30e453daea777443c4a37d2a09ad22
Jacky Fan <contact@jacky.fan>
2023-06-22 01:50:58 +0800

    fix: duplicated components in prod

--- pages/index.vue
+++ pages/index.vue
@@ -1,13 +1,12 @@
 <script lang="ts">
-import type { QueryBuilderParams } from '@nuxt/content/dist/runtime/types'
 
 export default {
   setup: async function () {
-    const host = "https://blog.jacky.fan"
-    let response = await fetch(`${host}/api/_content/query`, { method: "GET" })
-    let responseJSON = await response.json();
-    let articleLength = ref(responseJSON.length);
-    return { articleLength }
+    const { data, pending, error, refresh } = await useAsyncData("articles", async () => {
+      const articles = await queryContent("articles").sort({ created_date: -1 }).find()
+      return { articles: articles }
+    });
+    return { data }
   },
   data() {
     return {
@@ -20,10 +19,6 @@ export default {
       this.page = page;
       this.skipArticles = 5 * (page - 1);
     },
-    getArticleQuery: function () {
-      const query: QueryBuilderParams = { path: '/articles', limit: 5, skip: this.skipArticles, sort: [{ date: -1 }] }
-      return query;
-    },
     getPageTotal: function (articleLength: number): number {
       let total = Math.trunc(articleLength / 5);
       if ((articleLength % 5) > 0) total++;
@@ -31,6 +26,7 @@ export default {
     }
   }
 }
+
 </script>
 
 <template>
@@ -44,34 +40,32 @@ export default {
       <div class="grid grid-cols-1 lg:grid-cols-4 gap-4">
         <div class="col-span-3">
           <!-- Show article list -->
-          <ContentList path="/articles" :query="getArticleQuery()">
-            <template #default="{ list }">
-              <div class="bg-base-100 rounded-lg shadow-md my-4 px-8 py-12" v-for="article in list">
-                <article class="prose prose-slate w-full inline">
-                  <h2 class="mb-0">
-                    <NuxtLink :to="`${article._path}`" class="no-underline hover:text-blue-500">{{ article.title }}
-                    </NuxtLink>
-                  </h2>
-                  <small>
-                    <IconsDateIcon className="h-[1rem] mb-1 mr-1 inline" />{{ new
-                      Date(article.created_date).toDateString() }}
-                  </small>
-                  <p>
-                    <small>{{ article.description }}</small>
-                  </p>
-                </article>
+          <div>
+            <div v-for="(article, index) in data?.articles">
+              <div v-if="index < skipArticles + 5 && index >= skipArticles">
+                <div class="bg-base-100 rounded-lg shadow-md my-4 px-8 py-12">
+                  <article class="prose prose-slate w-full inline">
+                    <h2 class="mb-0">
+                      <NuxtLink :to="`${article._path}`" class="no-underline hover:text-blue-500">{{ article.title }}
+                      </NuxtLink>
+                    </h2>
+                    <small>
+                      <IconsDateIcon className="h-[1rem] mb-1 mr-1 inline" />{{ new
+                        Date(article.created_date).toDateString() }}
+                    </small>
+                    <p>
+                      <small>{{ article.description }}</small>
+                    </p>
+                  </article>
+                </div>
               </div>
-            </template>
-            <template #not-found>
-              <div class="bg-base-100 rounded-lg shadow-md my-4 px-8 py-12">
-                <p>No Blog Post Found.</p>
-              </div>
-            </template>
-          </ContentList>
+            </div>
+          </div>
           <div class="my-4 px-8 pt-12 lg:pb-4 text-center">
             <div class="join">
               <input class="join-item btn btn-square" type="radio" name="options" :aria-label="(n).toString()"
-                @click="() => { setPage(n) }" :checked="n === page" v-for="n in getPageTotal(articleLength)" />
+                @click="() => { setPage(n) }" :checked="n === page"
+                v-for="n in getPageTotal(data?.articles.length ?? 0)" />
             </div>
           </div>
         </div>
