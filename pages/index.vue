913b6c0d9b97c1ba32f3d9ccd20c3fd4c820b180
Jacky Fan <contact@jacky.fan>
2023-06-21 14:41:50 +0800

    fix: duplicated components in prod

--- pages/index.vue
+++ pages/index.vue
@@ -32,17 +32,16 @@ export default {
 </script>
 
 <template>
-  <div class="bg-base-200 min-h-screen">
-
-    <Head>
-      <Title>Jacky FAN's Blog</Title>
-    </Head>
-    <NavBar />
-    <main class="w-11/12 2xl:w-4/6 mx-auto py-8 min-h-screen">
-      <div class="grid grid-cols-1 lg:grid-cols-4 gap-4">
-        <div class="col-span-3">
-          <!-- Show article list -->
-          <ClientOnly>
+  <ClientOnly>
+    <div class="bg-base-200 min-h-screen">
+      <Head>
+        <Title>Jacky FAN's Blog</Title>
+      </Head>
+      <NavBar />
+      <main class="w-11/12 2xl:w-4/6 mx-auto py-8 min-h-screen">
+        <div class="grid grid-cols-1 lg:grid-cols-4 gap-4">
+          <div class="col-span-3">
+            <!-- Show article list -->
             <ContentList path="/articles" :query="getArticleQuery()">
               <template #default="{ list }">
                 <div class="bg-base-100 rounded-lg shadow-md my-4 px-8 py-12" v-for="article in list">
@@ -73,14 +72,14 @@ export default {
                   @click="() => { setPage(n) }" :checked="n === page" v-for="n in getPageTotal(articleLength)" />
               </div>
             </div>
-          </ClientOnly>
+          </div>
+          <div>
+            <AuthorPanel />
+            <LatestPostPanel />
+          </div>
         </div>
-        <div>
-          <AuthorPanel />
-          <LatestPostPanel />
-        </div>
-      </div>
-    </main>
-    <Footer />
-  </div>
+      </main>
+      <Footer />
+    </div>
+  </ClientOnly>
 </template>
