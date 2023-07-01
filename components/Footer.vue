fe84b35844e289a7351295fa2f81478411935122
Jacky Fan <contact@jacky.fan>
2023-07-02 02:09:14 +0800

    ui: small adjustment

--- components/Footer.vue
+++ components/Footer.vue
@@ -1,7 +1,7 @@
 <template>
     <footer class="footer footer-center p-4 bg-base-300 text-base-content">
         <div>
-            <p>© 2023 <a href="https://jacky.fan" target="_blank" class="hover:text-blue-500">Jacky FAN</a> </p>
+            <p>© 2023 <a href="https://jacky.fan" target="_blank" class="hover:text-blue-500">Jacky FAN's Blog.</a> </p>
         </div>
     </footer>
 </template>
--- components/NavBar.vue
+++ components/NavBar.vue
@@ -4,12 +4,10 @@
             <NuxtLink to="/" class="btn btn-ghost normal-case text-xl">Jacky FAN's Blog</NuxtLink>
         </div>
         <div class="flex-none">
-            <!-- <ul class="menu menu-horizontal px-1 hidden md:flex"> -->
-            <ul class="menu menu-horizontal px-1 hidden">
-                <li>
-                    <NuxtLink to="/" class="prose">Home</NuxtLink>
-                </li>
-                <li><a href="https://jacky.fan" target="_blank" class="prose">My Website</a></li>
+            <ul class="menu menu-horizontal px-1 hidden md:flex">
+                <!-- <li><a href="mailto:contact@jacky.fan"><IconsMail /></a></li> -->
+                <li><NuxtLink to="/" title="Home Page"><IconsHome /></NuxtLink></li>
+                <li><a href="/rss.xml" title="RSS Feed"><IconsRss /></a></li>
             </ul>
         </div>
     </div>
