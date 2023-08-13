461f35eab9d07f920ad726ff638fa2e6d51a93c8
Jacky Fan <contact@jacky.fan>
2023-08-14 00:30:08 +0800

    UI: update footer

--- components/Footer.vue
+++ components/Footer.vue
@@ -1,7 +1,7 @@
 <template>
     <footer class="footer footer-center p-4 bg-base-300 text-base-content">
         <div>
-            <p>© 2023 <a href="https://jacky.fan" target="_blank" class="hover:text-blue-500">Jacky FAN's Blog.</a> </p>
+            <p>© 2023 <a href="https://jacky.fan" target="_blank" class="hover:text-blue-500">Jacky FAN's Blog.</a> <a href="https://github.com/redfrogsss/nuxt-blog"  class="hover:text-blue-500" target="_blank">Source code available at Github</a></p>
         </div>
     </footer>
 </template>
