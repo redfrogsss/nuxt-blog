00b606676c333a54c39c9ee5baea559983703d95
Jacky Fan <contact@jacky.fan>
2023-11-15 00:26:32 +0800

    feat: count min read

---
+++ components/icons/Clock.vue
@@ -0,0 +1,16 @@
+<template>
+    <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor"
+        :class="className">
+        <path strokeLinecap="round" strokeLinejoin="round" d="M12 6v6h4.5m4.5 0a9 9 0 11-18 0 9 9 0 0118 0z" />
+    </svg>
+</template>
+<script>
+export default {
+    props: {
+        className: {
+            type: String,
+            default: 'w-6 h-6'
+        }
+    }
+}
+</script>
