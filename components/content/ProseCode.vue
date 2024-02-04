4cb13349174c7500b234915cadeee7936a3bab5c
Jacky Fan <contact@jacky.fan>
2024-02-04 19:33:59 +0800

    ui: add line number to code mockup

---
+++ components/content/ProseCode.vue
@@ -0,0 +1,52 @@
+<template>
+    <div class="mockup-code prose-mockup-code shadow">
+        <slot />
+    </div>
+</template>
+  
+<script setup lang="ts">
+defineProps({
+    code: {
+        type: String,
+        default: ''
+    },
+    language: {
+        type: String,
+        default: null
+    },
+    filename: {
+        type: String,
+        default: null
+    },
+    highlights: {
+        type: Array as () => number[],
+        default: () => []
+    },
+    meta: {
+        type: String,
+        default: null
+    }
+})
+</script>
+  
+<style>
+.prose-mockup-code pre {
+    padding-block: 0;
+    margin-block: 12px 4px;
+}
+
+.prose-mockup-code pre:before {
+    content: unset;
+}
+
+.prose-mockup-code pre code span.line {
+    padding-left: 8px;
+}
+
+.prose-mockup-code pre code span.line:before {
+    content: attr(line);
+    pointer-event: none;
+    margin-right: 20px;
+    opacity: .5;
+}
+</style>
